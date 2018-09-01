e5c64540ccbe097487d7b941642099a1340700ff
Leo Battle <leobattle@sun.home>
2018-09-01 10:45:47 +0100

    Added underscores to example programs to make them more readable

--- source/app.d
+++ source/app.d
@@ -2,20 +2,20 @@ import std.stdio;
 import ben_eater;
 
 ubyte[16] countTo255 = [
-	0b00100011, // Add 1 to regA
-	0b01010000, // Output regA
-	0b01100000, // JMP to top
-	0b00000001 // Gets added to regA
+	0b_0010_0011, // Add 1 to regA
+	0b_0101_0000, // Output regA
+	0b_0110_0000, // JMP to top
+	0b_0000_0001 // Gets added to regA
 ];
 
 // Same as countTo255 but stops when it reaches 255
 ubyte[16] stopAt255 = [
-	0b00100101, // Add 1 to regA
-	0b01010000, // Output regA
-	0b10000100, // If carryFlag go to HLT
-	0b01100000, // JMP to top
-	0b11110000, // HLT
-	0b00000001 // This gets added to regA
+	0b_0010_0101, // Add 1 to regA
+	0b_0101_0000, // Output regA
+	0b_1000_0100, // If carryFlag go to HLT
+	0b_0110_0000, // JMP to top
+	0b_1111_0000, // HLT
+	0b_0000_0001 // This gets added to regA
 ];
 
 void main() {
